9841c71031b6ba8bbefe995542562ddd69314cd0
Hyago Matos <hyago.v.a.m@gmail.com>
2022-01-06 13:09:59 -0400

    Project created

---
+++ src/main/java/com/hgp/contoladorveiculos/ControladorDeVeiculos2Application.java
@@ -0,0 +1,13 @@
+package com.hgp.contoladorveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControladorDeVeiculos2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControladorDeVeiculos2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hgp/contoladorveiculos/ControladorDeVeiculos2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgp.contoladorveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControladorDeVeiculos2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
