d05e6d410bda281afc95295e03a13c13983e3966
Hyago Matos <hyago.v.a.m@gmail.com>
2022-01-14 22:01:30 -0400

    Exception to unique fields

--- src/main/java/com/hgp/contoladorveiculos/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/hgp/contoladorveiculos/controllers/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -40,6 +41,19 @@ public class ResourceExceptionHandler {
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
 		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> Repitide(DataIntegrityViolationException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Repitide field");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		
